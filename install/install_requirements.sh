63129ceb9e3fb5548edc698202e7a5eb611d27fc
Serge Eric <sek@microcom.ca>
2024-01-22 00:47:46 -0500

    update jenkinsfile

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -2,9 +2,6 @@
 
 
 shopt -s globstar
-for i in **/*.txt; do # Whitespace-safe and recursive
-    process "$i"
-done
 for f in /odoo/custom_modules/16.0/*/requirement.txt; do
     pip install -r $f;
 done
