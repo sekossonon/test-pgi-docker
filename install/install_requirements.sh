d055ef83b294d0f0c06e012c4325a2e4cd87f8b9
Serge Eric <sek@microcom.ca>
2024-01-22 18:21:06 -0500

    update jenkinsfile

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -2,6 +2,6 @@
 
 shopt -s globstar nulljob dotglob
 
-for f in /odoo/custom_addons/16.0/*/requirement.txt; do
+for f in /odoo/custom_addons/16.0/*/requirements.txt; do
     pip install --no-cache-dir -r $f;
 done
