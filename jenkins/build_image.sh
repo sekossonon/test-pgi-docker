4eb40c9da07d0c9adeca3bfd411bc9d0c035c446
Serge Eric <sek@microcom.ca>
2024-01-22 00:32:49 -0500

    update jenkinsfile

--- jenkins/build_image.sh
+++ jenkins/build_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $DOCKERHUB_CREDS_PSW | docker login -u  $DOCKERHUB_CREDS_USR --password-stdin
-docker build -t my-pgi-16.0:$env.BUILD_ID .
+docker build -t my-pgi-16.0:$(env.BUILD_ID) .
