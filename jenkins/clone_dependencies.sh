4ad920b54d018b5dd01e436f627a6dd5f7d2f6e5
Serge Eric <sek@microcom.ca>
2024-01-21 23:45:05 -0500

    update jenkinsfile

--- jenkins/clone_dependencies.sh
+++ jenkins/clone_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'cloning odoo-common 16'
-echo git clone --branch=16.0 --depth=1 https://github.com/microcom/odoo-common.git
+git clone --branch=16.0 --depth=1 https://github.com/microcom/odoo-common.git
 mv ./odoo-common ./src/projects/
 echo 'clone oca/project'
 git clone --branch=16.0 --depth=1 https://github.com/OCA/project.git
