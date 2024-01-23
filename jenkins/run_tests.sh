a2810fa28e1696d5595ea3f82acd6a4e65fe3ff2
Serge Eric <sek@microcom.ca>
2024-01-23 14:35:02 -0500

    update jenkinsfile

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -4,5 +4,6 @@ container_name='odoo'
 if [ ! -z "$WORKSPACE" ]; then
     container_name=$WORKSPACE'_'$container_name
 fi
-
+echo $JOB_NAME
+echo $NODE_NAME
 docker exec -it $container_name -1 -u odoo sh -c "odoo-bin -d test_db -i microcom_ts --test-enable --stop-after-init"
