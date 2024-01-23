782ee566692c4cd5f48920776eb69b5d58d57f61
Serge Eric <sek@microcom.ca>
2024-01-23 00:11:58 -0500

    update jenkinsfile

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it odoo -u odoo sh -c "odoo-bin -d test_db -i microcom_ts --test-enable --stop-after-init"
+docker exec -it odoo-1 -u odoo sh -c "odoo-bin -d test_db -i microcom_ts --test-enable --stop-after-init"
