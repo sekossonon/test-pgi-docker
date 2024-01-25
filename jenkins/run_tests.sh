5511362e92ce4c67c8190b2e3b0433dfec155b4c
Serge Eric <sek@microcom.ca>
2024-01-24 21:29:23 -0500

    update jenkinsfile

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -7,7 +7,7 @@ if [ ! -z "$WORKSPACE" ]; then
     container_name="$(echo $WORKSPACE | rev | cut -d'/' -f 1 | rev)-$container_name-1"
 fi
 docker compose up -d
-echo $container_name
+docker ps
 sleep 5
 docker ps
 docker exec $container_name -1 -u odoo sh -c "odoo-bin -d test_db -i microcom_ts --test-enable --stop-after-init"
