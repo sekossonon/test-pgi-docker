7fd89867b231f8426cdb374cd19f4b17e4bd8f67
Serge Eric <sek@microcom.ca>
2024-01-25 23:43:20 -0500

    update jenkinsfile

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -3,12 +3,12 @@
 container_name='odoo'
 if [ ! -z "$WORKSPACE" ]; then
     # docker container name gets pre-pended with the jenkins workspace folder name
-    # so we need to do the same to be able to run the container
+    # so we need to do the same to get the real container name
     container_name="$(echo $WORKSPACE | rev | cut -d'/' -f 1 | rev)-$container_name-1"
 fi
 
 #docker compose build odoo # only destroy and rebuild odoo container
 docker compose up -d
-docker exec -u odoo $container_name sh -c "odoo --workers 0 -d test_db -i microcom_ts --test-enable --stop-after-init"
+docker exec -u odoo $container_name sh -c "odoo --workers 0 -d test_db -i microcom_ts --test-tags=/microcom_ts --stop-after-init"
 docker ps
 # docker ps -a -q -f name=jenkins-docker1
